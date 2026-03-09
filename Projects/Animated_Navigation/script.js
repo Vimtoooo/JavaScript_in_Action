9def6c82e1155ac2eb2b0d88fb15bad00a13ec1b
Vito Leonelli <leonellivito444@gmail.com>
2026-03-09 11:47:47 -0300

    Animated Navigation - Added Event Listeners

--- Projects/Animated_Navigation/script.js
+++ Projects/Animated_Navigation/script.js
@@ -1,3 +1,11 @@
 const sideNav = document.getElementById("sideNav");
 const openBtn = document.getElementById("openBtn");
 const closeBtn = document.getElementById("closeBtn");
+
+openBtn.addEventListener('click', function() {
+    sideNav.style.width = "250px";
+});
+
+closeBtn.addEventListener('click', function() {
+    sideNav.style.width = "0";
+});
